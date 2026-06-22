15236772067540f94dba5c83ee8f5a874268677b
Marco Fiordi <marco.fiordi97@gmail.com>
2026-06-22 10:48:16 +0200

    rimosso il prezzo in monete e aggiunto in euro, implementate modifiche a productdetails.css

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -60,98 +60,102 @@ function ProductDetails() {
             </main>
         );
     }
+    const formattedPrice = Number(product.price).toLocaleString("it-IT", {
+        style: "currency",
+        currency: "EUR"
+    });
 
     return (
-    <main className="product-details-page">
-        <section className="container">
-            <div className="product-details-card row g-4 align-items-center">
-                <div className="col-12 col-lg-6">
-                    <div className="product-image-wrapper">
-                        <img
-                            src={`/img/${product.image}`}
-                            alt={product.name}
-                            className="product-details-image"
-                        />
+        <main className="product-details-page">
+            <section className="container">
+                <div className="product-details-card row g-4 align-items-center">
+                    <div className="col-12 col-lg-6">
+                        <div className="product-image-wrapper">
+                            <img
+                                src={`/img/${product.image}`}
+                                alt={product.name}
+                                className="product-details-image"
+                            />
+                        </div>
                     </div>
-                </div>
 
-                <div className="col-12 col-lg-6">
-                    <div className="product-info">
-                        <span className="product-rarity">
-                            {product.rarity}
-                        </span>
-
-                        <h1 className="product-title">
-                            {product.name}
-                        </h1>
-
-                        <p className="product-description">
-                            {product.description}
-                        </p>
-
-                        <div className="product-price-box">
-                            <span className="product-price-label">
-                                Prezzo
+                    <div className="col-12 col-lg-6">
+                        <div className="product-info">
+                            <span className="product-rarity">
+                                {product.rarity}
                             </span>
 
-                            <p className="product-price">
-                                {product.price} monete d&apos;oro
+                            <h1 className="product-title">
+                                {product.name}
+                            </h1>
+
+                            <p className="product-description">
+                                {product.description}
                             </p>
+
+                            <div className="product-price-box">
+                                <span className="product-price-label">
+                                    Prezzo
+                                </span>
+
+                                <p className="product-price">
+                                    {formattedPrice}
+                                </p>
+                            </div>
+
+                            {product.categories?.length > 0 && (
+                                <div className="product-section">
+                                    <h5>Categorie</h5>
+
+                                    <div className="product-badges">
+                                        {product.categories.map((category) => (
+                                            <span
+                                                key={category.slug}
+                                                className="product-category-badge"
+                                            >
+                                                {category.name}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+
+                            {product.tags?.length > 0 && (
+                                <div className="product-section">
+                                    <h5>Tag</h5>
+
+                                    <div className="product-badges">
+                                        {product.tags.map((tag) => (
+                                            <span
+                                                key={tag.slug}
+                                                className="product-tag-badge"
+                                            >
+                                                {tag.name}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+
+                            <button
+                                type="button"
+                                className="product-cart-button"
+                                onClick={handleAddToCart}
+                            >
+                                Aggiungi all&apos;inventario
+                            </button>
+
+                            {cartMessage && (
+                                <p className="product-cart-message">
+                                    {cartMessage}
+                                </p>
+                            )}
                         </div>
-
-                        {product.categories?.length > 0 && (
-                            <div className="product-section">
-                                <h5>Categorie</h5>
-
-                                <div className="product-badges">
-                                    {product.categories.map((category) => (
-                                        <span
-                                            key={category.slug}
-                                            className="product-category-badge"
-                                        >
-                                            {category.name}
-                                        </span>
-                                    ))}
-                                </div>
-                            </div>
-                        )}
-
-                        {product.tags?.length > 0 && (
-                            <div className="product-section">
-                                <h5>Tag</h5>
-
-                                <div className="product-badges">
-                                    {product.tags.map((tag) => (
-                                        <span
-                                            key={tag.slug}
-                                            className="product-tag-badge"
-                                        >
-                                            {tag.name}
-                                        </span>
-                                    ))}
-                                </div>
-                            </div>
-                        )}
-
-                        <button
-                            type="button"
-                            className="product-cart-button"
-                            onClick={handleAddToCart}
-                        >
-                            Aggiungi all&apos;inventario
-                        </button>
-
-                        {cartMessage && (
-                            <p className="product-cart-message">
-                                {cartMessage}
-                            </p>
-                        )}
                     </div>
                 </div>
-            </div>
-        </section>
-    </main>
-);
+            </section>
+        </main>
+    );
 }
 
 export default ProductDetails;
