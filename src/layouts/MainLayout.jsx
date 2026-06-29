428c9187c7855297cc8cb9205a6b2e8448357429
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-06-29 15:47:28 +0200

    corretta home e aggiunte animazioni per le pagine

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,5 @@
-import { Outlet } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useLocation, useOutlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/index.css";
@@ -6,24 +7,54 @@ import NewsletterBanner from "../components/NewsletterBanner";
 import ChatbotWidget from "../components/ChatbotWidget";
 
 function MainLayout() {
+    const outlet = useOutlet();
+    const location = useLocation();
+    const timeoutRef = useRef(null);
+    const [displayedOutlet, setDisplayedOutlet] = useState(outlet);
+    const [displayedPathname, setDisplayedPathname] = useState(location.pathname);
+    const [transitionStage, setTransitionStage] = useState("route-enter");
+
+    useEffect(() => {
+        if (location.pathname === displayedPathname) {
+            setDisplayedOutlet(outlet);
+            return;
+        }
+
+        setTransitionStage("route-exit");
+
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+
+        timeoutRef.current = setTimeout(() => {
+            setDisplayedOutlet(outlet);
+            setDisplayedPathname(location.pathname);
+            setTransitionStage("route-enter");
+            timeoutRef.current = null;
+        }, 500);
+    }, [location.pathname, outlet, displayedPathname]);
+
+    useEffect(() => {
+        return () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+        };
+    }, []);
+
     return <>
-    
-        {/* <div className="main-background my-4 pb-5"> */}
+        <Header />
 
-            <Header />
+        <main className={`route-transition-container ${transitionStage}`}>
+            {displayedOutlet}
+        </main>
 
-            <Outlet />
+        <Footer />
 
+        <NewsletterBanner />
 
-            <Footer />
-
-            <NewsletterBanner />
-
-            <ChatbotWidget />
-
-        {/* </div> */}
-    </>
-    ;
+        <ChatbotWidget />
+    </>;
 }
 
 export default MainLayout;
